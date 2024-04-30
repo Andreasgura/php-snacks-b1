ed9b196f6a647f0e363ddce05440e729858a6fff
Andreasgura <andre-56@hotmail.it>
2024-04-30 16:26:18 +0200

    snack 1

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+$basketGames = [
+    [
+        "home team" => "Real Madrid",
+        "visiting team" => "Barcelona",
+        "home team score" => 77,
+        "visiting team score" => 50
+    ],
+    [
+        "home team" => "Milan",
+        "visiting team" => "Napoli",
+        "home team score" => 67,
+        "visiting team score" => 60
+    ],
+    [
+        "home team" => "Juventus",
+        "visiting team" => "Inter",
+        "home team score" => 80,
+        "visiting team score" => 70
+    ]
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+    <!-- <link rel="stylesheet" href="./css/style.css" /> -->
+</head>
+
+<body>
+    <div class="container mb-3 my-3 ">
+        <form action="index.php" method="POST">
+            <input type="text" class="form-control mb-3" name="toCensure" id="toCensure" >
+            <input type="submit" value="Invia" class="btn btn-primary my-3 ">
+        </form>
+    </div>
+</body>
+
+</html>
